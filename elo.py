798901bf7bc56e26dbe847fd464c9527caff0e3c
Carson Hu <carsonhu@g.ucla.edu>
2020-10-29 21:09:50 -0700

    added everything

---
+++ elo.py
@@ -0,0 +1,38 @@
+# Python 3 program for Elo Rating 
+import math 
+  
+# Function to calculate the Probability 
+def Probability(rating1, rating2): 
+  
+    return 1.0 * 1.0 / (1 + 1.0 * math.pow(10, 1.0 * (rating1 - rating2) / 400)) 
+  
+  
+# Function to calculate Elo rating 
+# K is a constant. 
+# d determines whether 
+# Player A wins or Player B.  
+def EloRating(Ra, Rb, K, d): 
+   
+  
+    # To calculate the Winning 
+    # Probability of Player B 
+    Pb = Probability(Ra, Rb) 
+  
+    # To calculate the Winning 
+    # Probability of Player A 
+    Pa = Probability(Rb, Ra) 
+  
+    # Case -1 When Player A wins 
+    # Updating the Elo Ratings 
+    if (d == 1) : 
+        Ra = Ra + K * (1 - Pa) 
+        Rb = Rb + K * (0 - Pb) 
+      
+  
+    # Case -2 When Player B wins 
+    # Updating the Elo Ratings 
+    else : 
+        Ra = Ra + K * (0 - Pa) 
+        Rb = Rb + K * (1 - Pb) 
+      
+    return Ra, Rb
